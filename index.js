851650a54ce2b27c44d791cbe59977c11ece9430
gunhee cho <ckhee93@gmail.com>
2017-06-12 02:35:34 +0900

    test sftp

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-
 var app = express();
 var server = http.createServer(app);
 
 app.use(express.static('./'));
 //app.use(express.static(path.join(__dirname, 'public')));
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     //res.send("hello world!");
     res.render("main.ejs");
 });
 
-app.get('/world.html',function(req,res){
+app.get('/world.html', function (req, res) {
     res.send("222");
 });
 
-server.listen(8080,function(){
+
+app.get('/ss',function(req,res){
+    res.send("hell");
+});
+
+server.listen(8080, function () {
     console.log('Express server listening on port ' + server.address().port);
+});
+
+
+let Client = require('ssh2-sftp-client');
+let sftp = new Client();
+sftp.connect({
+    host: 'jkhila.com',
+    port: '22',
+    username: 'gunny',
+    password: 'wh168168'
+}).then(() => {
+    return sftp.list('/home/gunny/Videos');
+}).then((data) => {
+    app.get('/sftp',function(req,res){
+        var a = "";
+        for(var i = 0;i<data.length;i++){
+            a += data[i].name+"\r\n";
+        }
+        res.send(a);
+    });
+    //console.log(data, 'the data info');
+}).catch((err) => {
+    console.log(err, 'catch error');
 });
